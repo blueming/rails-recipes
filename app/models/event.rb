226d928daf5133cdb3a176a739eace9509f6986f
blueming <wenyiff66@gmail.com>
2017-06-01 11:13:31 +0800

    plan two

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,14 @@ class Event < ApplicationRecord
 
  validates_presence_of :name
 
+ def to_param
+     self.friendly_id
+ end
+
+ protected
+
+   def generate_friendly_id
+     self.friendly_id ||= SecureRandom.uuid
+   end
+
 end
